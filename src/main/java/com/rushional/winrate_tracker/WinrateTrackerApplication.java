279d6243eed8b8ee308ca6f37c6fe164102ba928
Rushional <Rushional2@gmail.com>
2022-12-20 14:49:25 +0400

    first commit

---
+++ src/main/java/com/rushional/winrate_tracker/WinrateTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.winrate_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinrateTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinrateTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/winrate_tracker/WinrateTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.winrate_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinrateTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
